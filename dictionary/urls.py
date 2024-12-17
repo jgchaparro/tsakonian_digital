4e284ab451e4848c74828d46c683ecf22ef143c1
Jaime García Chaparro <jaime.garcia@globalservs.com>
2024-12-17 22:24:24 +0100

    CP commit

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -4,11 +4,10 @@ from . import views
 
 app_name = "dictionary"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="dictionary_index"),
     path("search/", views.search, name="search"),
     path("TS-EL/<str:entry>", views.tsakonian, name="tsakonian"),
     path("EL-TS/<str:entry>", views.greek, name="greek"),
     path("writing_extension/", views.writing_extension, name="writing_extension"),
     path("preservation_strategy/", views.preservation_strategy, name="preservation_strategy"),
-    path("chat/", views.chat, name="chat"),
 ]
