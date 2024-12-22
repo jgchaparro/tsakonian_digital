df8472592c95d6a8015fbd34a2331e4797a1225c
Jaime García Chaparro <jaime.garcia@globalservs.com>
2024-12-22 21:49:07 +0100

    Adding staticfiles

--- tsakonian_digital/urls.py
+++ tsakonian_digital/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('dictionary/', include('dictionary.urls')),
     path('translator/', include('translator.urls')),
     path('admin/', admin.site.urls),
+    path('', include('dictionary.urls')),
 ]
